5fe20ce11e161066acb5addae7dcebdcb68098f4
José Rodolfo Verduzco Torres <jverduzco@nearshoremx.com>
2018-09-14 13:23:02 -0500

    Removed unnessesary line

--- main.py
+++ main.py
@@ -8,8 +8,7 @@ def main():
     print("Welcome to the Caesar's Cipher\n")
 
     # Get the user Input
-    user_input = input("Please enter the message you want to encode: \n" +
-                       "(Write 'exit' to exit the program) \n\n" +
+    user_input = input("Please enter the message you want to encode: \n\n" +
                        "Message: ")
 
     print("Your written message is: " + user_input)
