bf5b048e47f8237787c96118def08881ef039863
José Rodolfo Verduzco Torres <jverduzco@nearshoremx.com>
2018-09-14 13:10:37 -0500

    Initial Commit

---
+++ main.py
@@ -0,0 +1,19 @@
+""" Caesar's Cipher main program """
+
+def main():
+    """" main
+
+    This function is the main function for the Caesar's Cipher program
+    """
+    print("Welcome to the Caesar's Cipher\n")
+
+    # Get the user Input
+    user_input = input("Please enter the message you want to encode: \n" +
+                       "(Write 'exit' to exit the program) \n\n" +
+                       "Message: ")
+
+    print("Your written message is: " + user_input)
+
+
+# Main Function
+main()
